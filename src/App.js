448065626995c0c13f9ec09732f2df528e3a9fac
Al-Amin <alaminkhanshakil@gmail.com>
2024-05-12 11:31:47 +0600

    Created the customer UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
+import CreateCustomer from "./components/CreateCustomer";
+import Customer from "./components/Customer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!</h1>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      <CreateCustomer />
+      <Customer />
     </div>
   );
 }
