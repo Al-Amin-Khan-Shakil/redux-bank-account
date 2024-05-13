9e0722492eb5087c8b89a84c5ef48cb34683db40
Al-Amin <alaminkhanshakil@gmail.com>
2024-05-13 22:31:35 +0600

    Fix devtools extention errors

--- src/store.js
+++ src/store.js
@@ -1,14 +1,17 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import composeWithDevTools from "redux-devtools-extension";
-import { thunk } from "redux-thunk";
-import accountReducer from "./slices/accountSlice";
-import customerReducer from "./slices/customerSlice";
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { thunk } from 'redux-thunk';
+import accountReducer from './slices/accountSlice';
+import customerReducer from './slices/customerSlice';
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;
